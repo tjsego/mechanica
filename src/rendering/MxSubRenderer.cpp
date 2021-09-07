38012a086ffbbf47db29a6e00ead5d721fafeec3
tjsego <tjsego@gmail.com>
2021-09-07 18:56:23 -0400

    Added helper class for modularizing renderering

---
+++ src/rendering/MxSubRenderer.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file MxSubRenderer.cpp
+ * @author T.J. Sego, Ph.D.
+ * @brief Defines interface for subrenderer children of MxUniverseRenderer
+ * @date 2021-09-07
+ * 
+ */
+#include "MxSubRenderer.h"
---
+++ src/rendering/MxSubRenderer.h
@@ -0,0 +1,50 @@
+/**
+ * @file MxSubRenderer.h
+ * @author T.J. Sego, Ph.D.
+ * @brief Defines interface for subrenderer children of MxUniverseRenderer
+ * @date 2021-09-07
+ * 
+ */
+#ifndef SRC_RENDERING_MXSUBRENDERER_H_
+#define SRC_RENDERING_MXSUBRENDERER_H_
+
+#include <mechanica_private.h>
+
+#include <rendering/ArcBallCamera.h>
+
+
+struct MxSubRenderer{
+
+    virtual ~MxSubRenderer() {}
+
+    /**
+     * @brief Starts the renderer. 
+     * 
+     * Called by parent renderer once backend is initialized. 
+     * 
+     * @param clipPlanes clip plane specification
+     * @return HRESULT 
+     */
+    virtual HRESULT start(const std::vector<MxVector4f> &clipPlanes) = 0;
+
+    /**
+     * @brief Updates visualization. 
+     * 
+     * @param camera scene camera
+     * @param viewportSize scene viewport size
+     * @param modelViewMat scene model view matrix
+     * @return HRESULT 
+     */
+    virtual HRESULT draw(Magnum::Mechanica::ArcBallCamera *camera, const MxVector2i &viewportSize, const MxMatrix4f &modelViewMat) = 0;
+
+    /**
+     * @brief Sets a clip plane equation
+     * 
+     * @param id id of clip plane equation
+     * @param pe clip plane equation
+     */
+    virtual void setClipPlaneEquation(unsigned id, const Magnum::Vector4& pe) {}
+
+};
+
+#endif // SRC_RENDERING_MXSUBRENDERER_H_
